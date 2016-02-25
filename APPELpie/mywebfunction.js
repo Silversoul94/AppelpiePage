5a1f938b3f6d332de4b8c940257719496ee7ef04
Henrik <henrikchiang@gmail.com>
2016-02-25 11:09:38 +0100

    now rating
    done with portion

--- APPELpie/mywebfunction.js
+++ APPELpie/mywebfunction.js
@@ -1,6 +1,16 @@
-var portion = 0;
-function setPortionSize() {
-    
-    
-    
+
+//var portionConvert = document.getElementsByClassName("mybutton").innerHTML;
+//var portion = parseInt(portionConvert .innerHTML);
+var previous = 1;
+
+function setPortionSize(tallrikar) {
+    PortionSize = document.getElementsByClassName("setSize");
+    for(var i = 0; i<11; i++){ 
+    var newPortionSize = parseFloat(PortionSize[i].innerHTML);
+    var twoPortion = (tallrikar * newPortionSize)/previous;  
+    //document.getElementsByClassName("setSize").innerHTML = twoPortion;
+    PortionSize[i].innerHTML = twoPortion;     
+    }
+    previous = tallrikar;
+    console.log("hej");
 }
