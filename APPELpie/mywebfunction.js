a227cefa4b5d0d32cc81ab3917c59912c8aa32c9
Henrik <henrikchiang@gmail.com>
2016-02-25 12:09:32 +0100

    some changes in html file

--- APPELpie/mywebfunction.js
+++ APPELpie/mywebfunction.js
@@ -1,16 +1,10 @@
-
-//var portionConvert = document.getElementsByClassName("mybutton").innerHTML;
-//var portion = parseInt(portionConvert .innerHTML);
 var previous = 1;
-
 function setPortionSize(tallrikar) {
     PortionSize = document.getElementsByClassName("setSize");
     for(var i = 0; i<11; i++){ 
     var newPortionSize = parseFloat(PortionSize[i].innerHTML);
     var twoPortion = (tallrikar * newPortionSize)/previous;  
-    //document.getElementsByClassName("setSize").innerHTML = twoPortion;
     PortionSize[i].innerHTML = twoPortion;     
     }
     previous = tallrikar;
-    console.log("hej");
 }
