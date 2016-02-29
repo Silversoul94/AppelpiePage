7b5da06c397ae55e1963beb465734b16fbcc0146
Henrik <henrikchiang@gmail.com>
2016-02-29 09:34:41 +0100

    star

--- APPELpie/rating.js
+++ APPELpie/rating.js
@@ -1,6 +1,4 @@
 // api key 9675310239642bc6
-
-
 //edu.oscarb.se/sjk15/api/recipe/?api_key=9675310239642bc6&recipe=appelpaj
 
 //edu.oscarb.se/sjk15/api/recipe/?api_key=9675310239642bc6&recipe=appelpaj&rating=4
@@ -8,12 +6,47 @@
 
 	$(".star").mouseover(function() {
 		$(this).attr("src","pic/star.png");
-            $(this).prevAll().attr("src","pic/star.png");
-            
+            $(this).prevAll().attr("src","pic/star.png");           
 			}
 );
 
+saveclick =  1;
+$(".star").click(function() {
+   // starpic = $(this).attr("id");
+         saveclick = 2;
+            (this).attr("src","pic/star.png");
+            $(starpic).prevAll().attr("src","pic/star.png");
+    }
+);
+
 $(".star").mouseout(function() {
-		$(this).attr("src","pic/graystar.png");
+		starpic=$(this).attr("src","pic/graystar.png");
+            $(this).prevAll().attr("src","pic/graystar.png");
+            console.log(saveclick);
+            if (saveclick == 2 ){
+                    starpic=$(this).attr("src","pic/star.png");
+                    $(starpic).prevAll().attr("src","pic/star.png");
+                }    
 			}
 );
+
+
+/*
+saveclick = 1;
+$(".star").click(function() {
+    starpic = $(this).attr("id");
+         saveclick = 2;
+       // $(starpic).prevAll().attr("src","pic/star.png");
+    }
+);
+$(".star").mouseout(function() {
+		$(this).attr("src","pic/graystar.png");
+            $(this).prevAll().attr("src","pic/graystar.png");
+            console.log(saveclick);
+            if (saveclick == undefined ){
+                    console.log(saveclick);
+                }    
+			}
+);
+
+*/
